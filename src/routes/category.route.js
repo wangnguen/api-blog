93cd2a53555d069cd4e6ed3d4cf65322c96cd659
Quang Nguyen <quang.nguyen13@ntq-solution.com.vn>
2025-10-22 09:39:04 +0700

    refactor(category): add verify

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,16 +1,22 @@
 const router = require("express").Router();
 
 const categoryController = require("../controllers/category.controller");
+const { verifyToken } = require("../middlewares/verifyToken");
+
 const catchAsync = require("../utils/catchAsync");
 
 router.get("/", catchAsync(categoryController.list));
 
+router.post("/", verifyToken, catchAsync(categoryController.createCategory));
+
 router.get("/:slug", catchAsync(categoryController.getBySlug));
 
-router.post("/", catchAsync(categoryController.createCategory));
+router.patch("/:id", verifyToken, catchAsync(categoryController.editCategory));
 
-router.patch("/:id", catchAsync(categoryController.editCategory));
-
-router.delete("/:id", catchAsync(categoryController.deleteCategory));
+router.delete(
+	"/:id",
+	verifyToken,
+	catchAsync(categoryController.deleteCategory),
+);
 
 module.exports = router;
